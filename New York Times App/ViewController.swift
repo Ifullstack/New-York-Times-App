ea714136f581d4f1fbb28a4a8cd2ce29e62f1b02
Cane Allesta <idevapps19@gmail.com>
2021-12-28 18:04:19 +0100

    base project created

---
+++ New York Times App/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  New York Times App
+//
+//  Created by Cane Allesta on 28/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
